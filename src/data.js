ae41608707888dee96779d6c5a37f7d0ea3f81d3
Victor <vbasov10@gmail.com>
2021-08-19 21:15:42 -0400

    update resume

--- src/data.js
+++ src/data.js
@@ -29,8 +29,8 @@ export const featuredPortfolio = [
       title: "Maps App",
       img:
         "https://cdn.shopify.com/s/files/1/0101/2792/products/Under_Construction_copy_600x600_crop_center.jpg?v=1569104295",
-        githublink: "",
-        linktoproject: "",
+        githublink: "https://github.com/victor-basov/ratemydestination",
+        linktoproject: "https://ratemydestination.herokuapp.com/",
 
 
     },
