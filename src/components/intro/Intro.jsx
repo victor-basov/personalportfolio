3779d46f05e33cb7c97cdf8452e63b341ab55859
Victor <vbasov10@gmail.com>
2021-11-28 14:37:20 -0500

    headline update and resume update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -11,7 +11,7 @@ useEffect(()=>{
         showCursor: true,
         backDelay: 1500,
         backSpeed: 60,
-            strings: ['Photographer 📷 ', ' Foodie 🍣', 'Web Developer at Soterix Medical 😈'],
+            strings: ['Photographer 📷 ', ' Foodie 🍣', 'Software Developer at MetTel 😈'],
         });
 
 },[])
